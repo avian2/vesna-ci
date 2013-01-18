b92aed51fe4450d598777bdce8c51d7af59f4bbc
Tomaz Solc <tomaz.solc@tablix.org>
2013-01-18 15:26:17 +0100

    Added documentation.

--- settings.py
+++ settings.py
@@ -1,7 +1,16 @@
 import os
 
+# URL where logs will be accessible so that they can be properly linked
+# from commit status messages.
+#
+# "http://foo/bar" means that logs should be accessible at
+# "http://foo/bar/logs/build.*.html".
+#
+# WARNING: Make sure that "vesna-ci" directory itself isn't available over
+# the web as it contains files with sensitive information!
 BASE_URL = "http://log-a-tec.eu/vesna-ci"
 
 BASE_DIR = os.path.dirname(__file__)
 
+# Github repository that will be checked for open pull requests.
 REPO = "sensorlab/vesna-drivers"
